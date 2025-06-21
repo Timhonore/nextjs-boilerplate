6a1910cf6b53f5d70877e6b6f8ff071d61b6fcc2
Timhonore <94400656+Timhonore@users.noreply.github.com>
2025-06-21 21:58:40 +0200

    Add files via upload

---
+++ app/checkin/page.js
@@ -0,0 +1,3 @@
+export default function CheckIn() {
+  return <div className="p-4">Check-in modul: Nummerpladebaseret tjek-ind vises her.</div>;
+}
---
+++ app/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-2xl font-bold p-4">Velkommen til Skærmstyring.dk</h1>;
+}
