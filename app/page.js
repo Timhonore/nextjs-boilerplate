8db8739932e7793a5cbdc13c81735d428b451108
Timhonore <94400656+Timhonore@users.noreply.github.com>
2025-06-22 09:44:20 +0200

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,3 +1,1 @@
-export default function Home() {
-  return <h1 className="text-2xl font-bold p-4">Velkommen til Skærmstyring.dk</h1>;
-}
+export default function Page() { return <div>Velkommen til Skærmstyring.dk</div>; }
